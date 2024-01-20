0d88feb3ae9bfc6c1c92b1e656827bb157105992
MonaliDhakate <143076144+MonaliDhakate@users.noreply.github.com>
2024-01-20 11:46:25 +0530

    Add files via upload

---
+++ Withdraw.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankingProjectConsole
+{
+    public class Withdraw:Credit
+    {
+        Checkbalance ck=new Checkbalance(); 
+        public void withdraw(double bal)
+        {
+            
+            Console.WriteLine("please enter account no");
+            double acc=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter otp");
+            int otp= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter how many amount u want to withdraw");
+            double money = Convert.ToDouble(Console.ReadLine());
+
+            double tot = bal - money;
+            
+
+            if (tot < 0)
+            {
+                Console.WriteLine("please enter valid amount");
+            }
+            else
+            {
+                Console.WriteLine($"Your succefully withdraw of amount {money}");
+            }
+           
+            ck.balance(tot);
+
+        }
+    }
+}
