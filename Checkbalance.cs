c6243b04d51e17db07dfff276417ec4828a46e87
MonaliDhakate <143076144+MonaliDhakate@users.noreply.github.com>
2024-01-20 11:45:47 +0530

    Add files via upload

---
+++ Checkbalance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankingProjectConsole
+{
+    public class Checkbalance: Credit
+    {
+       
+       public void balance(double credit)
+        {
+            
+            double balanced = credit;
+            Console.WriteLine($"Your available balanced is {balanced}");
+            Console.WriteLine("Thank you..!!");
+            //Withdraw wt = new Withdraw();
+            //wt.withdraw(balanced);
+        }
+
+        
+    }
+}
